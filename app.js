69050e2f39d9914a156e8c63407ec0bfdc7541aa
Jyothish Jose <jyothish6190@gmail.com>
2016-10-11 20:02:22 +0530

    bug fixed

--- app.js
+++ app.js
@@ -21,40 +21,65 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(__dirname + '/public'));
+var multer = require('multer');
+var port = 3000;
 
-app.use('/', routes);
-app.use('/users', users);
+app.set('port', port); 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+/* Disk Storage engine of multer gives you full control on storing files to disk. The options are destination (for determining which folder the file should be saved) and filename (name of the file inside the folder) */
+
+var storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    callback(null, 'public/uploads');
+  },
+  filename: function (request, file, callback) {
+    console.log(file);
+    callback(null, Date.now() + '-' + file.originalname);
+  }
 });
 
-// error handlers
+/*Multer accepts a single file with the name photo. This file will be stored in request.file*/
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+var upload = multer({storage: storage}).any();
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
+//Showing index.html file on our homepage
+app.get('/', function(resuest, response) {
+      response.sendFile(path.join(__dirname + '/file.html'));
+});
 
-module.exports = app;
+//Posting the file upload
+app.post('/upload', function(request, response) {
+  upload(request, response, function(err) {
+  if(err) {
+    console.log(err);
+    return;
+  }
+  console.log(request.file);
+  console.log(request.files);
+  response.json({'path' : request.files[0].path});
+  console.log('Photo Uploaded');
+  })
+});
+
+var server = app.listen(port, function () {
+  console.log('Listening on port ' + server.address().port)
+});
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,29 @@ var multer = require('multer');
 var path = require("path");
 var router = express.Router();
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads')
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, 'public/uploads');
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
   }
 });
-
-var upload = multer({ storage: storage});
+var upload = multer({ storage : storage}).single('userPhoto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
         res.sendFile(path.join(__dirname + '/file.html'));
 });
 
-router.post('/upload', upload.any(), function (req, res, next) {
-  res.send(req.files);
+router.post('/upload',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            console.log(err);
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
 });
 
 module.exports = router;
